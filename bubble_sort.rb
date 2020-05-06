e2521774ed3edc4bbce1916d91a815e56284ac1d
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-05-06 14:17:09 +0000

    Add another implementation to the file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,5 +16,24 @@ def bsort(arr)
 return sorted
 end
 
+# Second implementation
+
+def bubble_sort(arr = [2, 4, 1, 5, 3, 10, 6])
+    n = arr.length
+    loop do
+      (n-1).times do |i|
+        if (arr[i] <=> arr[i+1]) == 1
+          arr[i], arr[i+1] = arr[i+1], arr[i]
+        end
+      end
+      n -= 1
+      break if n <= -1
+    end
+    return arr
+  end
+  
+  p bubble_sort
+  
+
 print bsort([0, 6, 10, 5, 7, 1, 3, 4, 8, 9])
 
