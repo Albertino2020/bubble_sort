4d7ad4c5557395722ec3f691d974ed802bdd6778
Albertino Francisco <albertino@ua.pt>
2020-05-06 14:38:37 +0100

    fixed bubble_sort.rb errors.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,20 @@
-def bsort (arr)
+def bsort(arr)
     sorted = []
-    n= (arr.length) -1
-
-   while n > 0 do 
-    for i in arr[0..n] do
-        temp=arr[i] <=> arr[i+1]
-       if temp==1
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-        end   
+    n= (arr.length) - 1
+    while n >= 0 do 
+    for i in 0..n-1
+        j = i+1
+        temp = arr[i] <=> arr[j]
+       if temp == 1
+            arr[i], arr[j] = arr[j], arr[i]
+        end
+    end      
+     sorted[n] = arr[n] 
+     n -= 1               
     end
-    n-=1
-   end
 
+return sorted
 end
 
-puts bsort([0, 6, 10, 5, 7, 1, 3, 4, 8, 9])
+print bsort([0, 6, 10, 5, 7, 1, 3, 4, 8, 9])
 
