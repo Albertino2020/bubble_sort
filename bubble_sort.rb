42d28322e44e9e92686778e4035a2a460c8186bf
Albertino Francisco <albertino@ua.pt>
2020-05-07 08:39:11 +0100

    Fixed linting errors.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,16 @@
+# frozen_string_literal: true
+
 def bubble_sort_1(arr)
-  sorted = []
   n = arr.length - 1
   while n >= 0
     0.upto(n - 1) do |i|
       j = i + 1
       temp = arr[i] <=> arr[j]
-      if temp.positive?
-        arr[i], arr[j] = arr[j], arr[i]
-      end
+      arr[i], arr[j] = arr[j], arr[i] if temp.positive?
     end
-    sorted[n] = arr[n]
     n -= 1
   end
-  sorted
+  arr
 end
 
 p bubble_sort_1([0, 6, 10, 5, 7, 1, 3, 4, 8, 9])
@@ -23,9 +21,7 @@ def bubble_sort_2(arr = [2, 4, 1, 5, 3, 10, 6])
   n = arr.length
   loop do
     (n - 1).times do |i|
-      if (arr[i] <=> arr[i + 1]) == 1
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      end
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if (arr[i] <=> arr[i + 1]) == 1
     end
     n -= 1
     break if n <= -1
@@ -46,7 +42,8 @@ def bubble_sort_by(arr)
       b = arr[i + 1]
       temp = yield(a, b)
       if temp.positive?
-        arr[i], arr[i + 1] = b, a
+        arr[i] = b
+        arr[i + 1] = a
       end
     end
     sorted[n] = arr[n]
@@ -56,4 +53,4 @@ def bubble_sort_by(arr)
   sorted
 end
 
-p bubble_sort_by (["hi", "hello", "hey"]) { |left, right| left.length - right.length }
+p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
